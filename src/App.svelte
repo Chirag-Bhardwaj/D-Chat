6fe83ad2e12fc8c5468685961bb8203d30696022
Chirag-Bhardwaj <chiragbhardwaj2002@gmail.com>
2022-10-26 22:07:30 -0500

    Added svelte template source files

--- src/App.svelte
+++ src/App.svelte
@@ -3,8 +3,7 @@
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<h1>D-Chat!</h1>
 </main>
 
 <style>
